4c1bb5c8ecee16a30f42ae368bf3553e111b4a4a
Curtis Hanson <chanson@mesd.k12.or.us>
2014-03-03 15:21:59 -0800

    updated stuff

---
+++ Twig/IntervalExtension.php
@@ -0,0 +1,36 @@
+<?php
+// MESDPresentation/PresentationBundle/Twig/IntervalExtension.php
+namespace MESD\Presentation\PresentationBundle\Twig;
+
+use Twig_Extension;
+
+class IntervalExtension extends Twig_Extension
+{
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('diff', array($this, 'intervalFunction')),
+        );
+    }
+
+    public function intervalFunction($dt1, $dt2, $format = null)
+    {
+        $dt1 = new \DateTime($dt1);
+        $dt2 = new \DateTime($dt2);
+
+        $interval = $dt2->diff($dt1);
+
+        if(empty($format)){
+            return $interval->format('%Y-%M-%D %H:%I:%S %R');
+        }
+        else{
+            return $interval->format($format);
+        }
+    }
+
+    public function getName()
+    {
+        return 'interval_extension';
+    }
+}
