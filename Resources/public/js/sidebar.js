cd8386f99a90a66d50d1e2c9557a242c16e16496
lighthart <dlighthart@mesd.k12.or.us>
2017-08-31 11:50:46 -0700

    shim ou tnon existent sidebar

--- Resources/public/js/sidebar.js
+++ Resources/public/js/sidebar.js
@@ -5,25 +5,32 @@
 // inhibits recurring callback for duration of quiet before executing
 var delay = (function() {
     var timer = 0;
-    return function(callback, ms){
+    return function(callback, ms) {
         clearTimeout(timer);
         timer = setTimeout(callback, ms);
     };
 })();
 
 toggleSidebar = function() {
-    $('.sidebar').toggleClass('sidebar-closed');
-    $('.drag-bar').toggleClass('sidebar-closed');
-    $('.container-inner').toggleClass('sidebar-closed');
+    $('.sidebar')
+        .toggleClass('sidebar-closed');
+    $('.drag-bar')
+        .toggleClass('sidebar-closed');
+    $('.container-inner')
+        .toggleClass('sidebar-closed');
 
     // toggling
-    $.cookie('sidebar-closed', (parseInt($.cookie('sidebar-closed'),10) ? 0 : 1), {path: '/', expires: 30} );
+    $.cookie('sidebar-closed', (parseInt($.cookie('sidebar-closed'), 10) ? 0 : 1), {
+        path: '/',
+        expires: 30
+    });
 }
 
-$('.drag-bar-handle, .sidebar-extra').on('click', function(e) {
-    e.preventDefault();
-    toggleSidebar();
-});
+$('.drag-bar-handle, .sidebar-extra')
+    .on('click', function(e) {
+        e.preventDefault();
+        toggleSidebar();
+    });
 
 // $('.drag-bar').mousedown(function(e){
 //     e.preventDefault();
@@ -54,27 +61,33 @@ $('.drag-bar-handle, .sidebar-extra').on('click', function(e) {
 //     }
 // });
 
-$(document).mouseup(function(e){
-    // get window width
-    var vp = $(window).viewportW();
+$(document)
+    .mouseup(function(e) {
+        // get window width
+        var vp = $(window)
+            .viewportW();
 
-    if (vp < mdMin) {
-        sidebar.lastPos = sidebar.currPos;
-    } else {
-        // do nothing
-    }
-    $(document).unbind('mousemove');
-});
+        if (typeof sidebar !== 'undefined' && vp < mdMin) {
+            sidebar.lastPos = sidebar.currPos;
+        } else {
+            // do nothing
+        }
+        $(document)
+            .unbind('mousemove');
+    });
 
 // when sidebar is scrolled, set a cookie of the scroll position
-$('#sidebar').scroll(function() {
-    $.cookie('sscroll', $('#sidebar').scrollTop());
-});
+$('#sidebar')
+    .scroll(function() {
+        $.cookie('sscroll', $('#sidebar')
+            .scrollTop());
+    });
 
-
-$(document).ready(function() {
-    // on page load, if sidebar cookie is scrolled, go back to position
-    if ($.cookie('sscroll') !== null) {
-        $('#sidebar').scrollTop($.cookie('sscroll'));
-    }
-});
+$(document)
+    .ready(function() {
+        // on page load, if sidebar cookie is scrolled, go back to position
+        if ($.cookie('sscroll') !== null) {
+            $('#sidebar')
+                .scrollTop($.cookie('sscroll'));
+        }
+    });
