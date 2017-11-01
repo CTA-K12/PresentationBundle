0742c2eb3cd2deb5bc1d52a77ce1698cab8e0013
Curtis G Hanson <curtisghanson@gmail.com>
2017-10-31 21:39:46 -0700

    update presentation to latest js libs

---
+++ Command/GenerateViewsCommand.php
@@ -0,0 +1,48 @@
+<?php
+namespace Mesd\PresentationBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Finder\Finder;
+
+class GenerateViewsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('mesd:presentation:generate')
+            ->setDescription('Generate templates into app/Resources for override')
+            ->addOption('force', 'f', InputOption::VALUE_OPTIONAL, 'Force the generation of views')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $fs     = new Filesystem;
+        $finder = new Finder;
+        $appDir = $this->getContainer()->getParameter('kernel.root_dir');
+        $srcDir = $appDir . '/../src/Mesd/PresentationBundle/Resources/public';
+        $vndDir = $appDir . '/../vendor/mesd/presentation-bundle/Mesd/PresentationBundle/Resources/public';
+        $webDir = $appDir . '/../web';
+
+        if ($fs->exists($appDir))
+        {
+            $finder->in($appDir);
+
+            foreach ($finder as $obj)
+            {
+                # code...
+            }
+        }
+        elseif ($fs->exists($vndDir))
+        {
+
+        }
+        $output->writeln('Starting plublish.');
+    }
+}
---
+++ Resources/public/assets/js/app.js
@@ -0,0 +1,24 @@
+// assets/js/main.js
+window.$ = window.jQuery = require('jquery');
+//window.moment = require('moment');
+//window._ = require('underscore');
+
+// loads the jquery package from node_modules
+//import $ from 'jquery';
+//import Vue from 'vue';
+
+// import the function from greet.js (the .js extension is optional)
+// ./ (or ../) means to look for a local file
+// var greet = require('./greet');
+
+//$(document).ready(function() {
+//    $('h2').html(greet('john'));
+//});
+
+//var app = new Vue({
+//    el: '#app',
+//    components: {
+//        Library
+//    }
+//});
+
